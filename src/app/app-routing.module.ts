99135f5dae183fbffc289717cebe6c8b15bce52b
Classic Values <71681815+classicvalues@users.noreply.github.com>
2022-05-14 00:40:08 -0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,8 +12,16 @@ const routes: Routes = [
     loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
   },
   {
+    path: 'support',
+    loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule)
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'app',
+    loadChildren: () => import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
   },
   {
     path: 'signup',
